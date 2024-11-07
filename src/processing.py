b420eb41c5b12946644c6b278de7aa7fdf4793bf
Yulia Samoylova <u.u.samoylova@gmail.com>
2024-11-07 23:01:03 +0500

    add file processing

---
+++ src/processing.py
@@ -0,0 +1,11 @@
+from typing import List, Dict, Any
+
+
+def filter_by_state(list_dict: List[Dict[str, Any]], state='EXECUTED') -> List[Dict[str, Any]]:
+    """Функция возвращает новый список словарей [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}], содержащий только те словари, у которых ключ
+state соответствует указанному значению."""
+
+
+def sort_by_date(list_dict: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    """Функция должна возвращать новый список, отсортированный по дате (date)"""
