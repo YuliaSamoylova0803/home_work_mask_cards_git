115cc9532c9842f8b69b13f16749e91a56f058e7
Yulia Samoylova <u.u.samoylova@gmail.com>
2024-11-09 00:35:35 +0500

    check flake8

--- src/processing.py
+++ src/processing.py
@@ -2,8 +2,7 @@ from typing import List, Dict, Any
 
 
 def filter_by_state(list_dict: List[Dict[str, Any]], state='EXECUTED') -> List[Dict[str, Any]]:
-    """Функция возвращает новый список словарей [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}], содержащий только те словари, у которых ключ
+    """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ
 state соответствует указанному значению."""
     new_list_dict = []
     for i in list_dict:
@@ -17,5 +16,3 @@ def sort_by_date(list_dict: List[Dict[str, Any]], reverse: bool = True) -> List[
     sorted_list_dict = []
     sorted_list_dict = sorted(list_dict, key=lambda x: x["date"], reverse=reverse)
     return sorted_list_dict
-
-#print(sort_by_date())
