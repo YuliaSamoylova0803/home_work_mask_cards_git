5e12160a9cd7fbe2d02e61b7f93e8ff58b1ac652
Yulia Samoylova <u.u.samoylova@gmail.com>
2024-11-08 23:57:41 +0500

    add function filter by state

--- src/processing.py
+++ src/processing.py
@@ -5,6 +5,11 @@ def filter_by_state(list_dict: List[Dict[str, Any]], state='EXECUTED') -> List[D
     """Функция возвращает новый список словарей [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
      {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}], содержащий только те словари, у которых ключ
 state соответствует указанному значению."""
+    new_list_dict = []
+    for i in list_dict:
+        if i.get("state") == state:
+            new_list_dict.append(i)
+    return new_list_dict
 
 
 def sort_by_date(list_dict: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
