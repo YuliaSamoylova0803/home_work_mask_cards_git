62a55a3a810a18734eacdae21663b7e275e6f672
Yulia Samoylova <u.u.samoylova@gmail.com>
2024-11-18 22:55:12 +0500

    add generatiors file and update conftest

---
+++ src/generators.py
@@ -0,0 +1,17 @@
+from typing import List, Dict, Any
+
+
+def filter_by_currency(transactions: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    """Функция должна возвращать итератор, который поочередно выдает транзакции,
+    где валюта операции соответствует заданной (например, USD)"""
+    pass
+
+
+def transaction_descriptions(transactions: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+    """Функция принимает список словарей с транзакциями и возвращает описание каждой операции по очереди."""
+    pass
+
+
+
+
+
