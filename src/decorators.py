dcaffc23fd830879ec72cbd671952707df01c72c
Yulia Samoylova <u.u.samoylova@gmail.com>
2024-11-29 17:24:08 +0500

    tests and all files

--- src/decorators.py
+++ src/decorators.py
@@ -31,24 +31,6 @@ def log(filename: Optional[str]=None) -> Callable:
     return logging_decorator
 
 
-def printing(func):
-    def wrapper(*args, **kwargs):
-        print(f"Function {func} started")
-        result = func(*args, **kwargs)
-        print(f"Function {func} finished")
-        return result
-    return wrapper
-
-
-def timer(func):
-    def wrapper(*args, **kwargs):
-        time_1 = time()
-        result = func(*args, **kwargs)
-        time_2 = time()
-        print(f"Time for work: {time_2 - time_1}")
-    return wrapper
-
-
 @log(filename="my_logs.txt")
 def my_function(x: int | str, y: int | str) -> int:
     """Функция суммирует два числа и возвращает результат"""
@@ -58,11 +40,7 @@ def my_function(x: int | str, y: int | str) -> int:
         print("Ошибка ввода! Пожалуйста, вводите только целые числа.")
         raise e
 
-# @log(filename="my_logs.txt")
-# def my_function(x: int | str, y: int | str) -> int:
-#     """Функция суммирует два числа и возвращает результат"""
-#     return int(x) + int(y)
 
-my_function(1, 2)
-print(my_function(1,2))
+# my_function(1, 2)
+# print(my_function(1,2))
 
--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,3 +1,5 @@
+from fileinput import filename
+
 import pytest
 from src.decorators import log, my_function
 
@@ -13,11 +15,6 @@ def test_log_console_ok(capsys):
     assert captured.out == "my_function ok\n"
 
 
-def test_crash_log() -> None:
-    with pytest.raises(ValueError, match="invalid literal for int() with base 10: 'a'"):
-        my_function()
-
-
 def test_log_captured(capsys) -> None:
     @log(filename=None)
     def my_function(x: int, y: int) -> int:
@@ -27,3 +24,27 @@ def test_log_captured(capsys) -> None:
     my_function(2, 2)
     captured = capsys.readouterr()
     assert captured.out == "my_function ok\n"
+
+
+def test_log_in_file() -> None:
+    @log(filename="log.txt")
+    def my_function(x:int, y:int) -> int:
+        """Функция суммирует два числа и возвращает результат"""
+        return x + y
+    my_function(2, 2)
+    with open("log.txt") as f:
+        data = f.read().split("\n")[-2]
+    assert data == "my_function ok"
+
+
+def test_error_handling(capsys):
+    with pytest.raises(TypeError):
+        my_function(1, 'a')
+    captured = capsys.readouterr()
+    assert "my_function error: ValueError. Inputs: (1, 'a'), {}" in captured.out
+
+
+
+def test_crash_log() -> None:
+    with pytest.raises(Exception, match="invalid literal for int() with base 10: 'a'"):
+        my_function()
