358aeadd78a6251421b164256c14f80728d1db8e
Yulia Samoylova <u.u.samoylova@gmail.com>
2024-12-04 10:58:22 +0500

    update func get lict dict financial transaction

--- src/utils.py
+++ src/utils.py
@@ -4,12 +4,15 @@ import json
 # load_dotenv('.env')
 
 
-def get_list_dict_with_financial_transaction_data(file_operations) -> List:
+def get_list_dict_with_financial_transaction_data(file_operations) -> List[Any]:
     """Функция, возвращает список словарей с данными о финансовых транзакциях"""
-    with open('data/operations.json', encoding='utf-8') as file_operations:
-        data = json.load(file_operations)
-        return data
+    if file_operations is None:
+        return []
+    else:
+        with open('data/operations.json', encoding='utf-8') as file_operations:
+            dict_transaction_data = json.load(file_operations)
+            return dict_transaction_data
 
 
 
-# print(get_list_dict_with_financial_transaction_data(file_operations='data/operations.json'))
+print(get_list_dict_with_financial_transaction_data(file_operations='data/operations.json'))
