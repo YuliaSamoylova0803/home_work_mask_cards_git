989dda76f684c0c7a08d1c878bca93f758a260f8
Yulia Samoylova <u.u.samoylova@gmail.com>
2024-12-09 14:27:06 +0500

    update masks.py

--- src/masks.py
+++ src/masks.py
@@ -11,7 +11,7 @@ abs_log_file_path = os.path.abspath(rel_log_file_path)
 
 logger = logging.getLogger("masks")
 logger.setLevel(logging.INFO)
-file_handler = logging.FileHandler(rel_log_file_path, "w", encoding="utf-8")
+file_handler = logging.FileHandler(abs_log_file_path, "w", encoding="utf-8")
 file_formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s: %(message)s")
 file_handler.setFormatter(file_formatter)
 logger.addHandler(file_handler)
