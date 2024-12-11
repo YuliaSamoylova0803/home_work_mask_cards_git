b7c4d94aa938e873b27d1f86c7907359c1a33db4
Yulia Samoylova <u.u.samoylova@gmail.com>
2024-12-11 22:39:26 +0500

    update files csv pandas and tests

--- src/csv_pandas.py
+++ src/csv_pandas.py
@@ -5,23 +5,43 @@ import pandas as pd
 
 # Получаем абсолютный путь до текущей директории
 current_dir = os.path.dirname(os.path.abspath(__file__))
-
-# Создаем путь до файла cvs относительно текущей директории
 rel_src_file_path = os.path.join(current_dir, "../data/transactions.csv")
 abs_src_file_path = os.path.abspath(rel_src_file_path)
+
 
 def get_csv_data(path_csv: str) -> list[dict]:
     """Функция для считывания финансовых операций из CSV выдает список словарей с транзакциями"""
     returned_lict = []
-
     try:
-        with open(path_csv) as file_save_csv:
-            reader_dict_csv = csv.DictReader(file_save_csv, delimiter=";")
-            return list(reader_dict_csv)
+        transactions_df = pd.read_csv(path_csv, delimiter=";", encoding="utf-8")
+        result = transactions_df.to_dict(orient="records")
+        return result
 
     except FileNotFoundError:
+        return []
+    except Exception as e:
         return []
 
 
 print(get_csv_data(abs_src_file_path))
 print(get_csv_data(path_csv="data/empty.json"))
+
+
+# Получаем абсолютный путь до текущей директории
+current_dir = os.path.dirname(os.path.abspath(__file__))
+
+# Создаем путь до файла cvs относительно текущей директории
+rel_src_file_path1 = os.path.join(current_dir, "../data/transactions_excel.xlsx")
+abs_src_file_path1 = os.path.abspath(rel_src_file_path1)
+
+
+def get_excel_data(path_excel: str) -> list[dict]:
+    """Функция для считывания финансовых операций из Excel выдает список словарей с транзакциями."""
+    try:
+        df = pd.read_excel(path_excel)
+    except FileNotFoundError:
+        return []
+    excel_data = df.to_dict(orient="records")
+    return excel_data
+
+print(get_excel_data(abs_src_file_path1))
